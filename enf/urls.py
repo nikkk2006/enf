a10b96764a20a425199b13251ed4ff5766ca8e18
nikkk2006 <hlinski220706@gmail.com>
2026-01-21 15:37:17 +0300

    добавили приложение cart и его функционал

--- enf/urls.py
+++ enf/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     # Включение URL-ов приложения 'main' с пространством имен 'main'
     path('', include('main.urls', namespace='main')),
 ]
